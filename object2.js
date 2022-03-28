c1e4a1b9d81ede57b69783a219dec346a261dc48
maoma1 <jenniensofor@gmail.com>
2022-03-28 16:28:25 +0100

    first object2 commit

--- object2.js
+++ object2.js
@@ -1,46 +1,30 @@
-
-
 /*Create an array of people objects with first name, last name, and age, then write a JavaScript
  function that takes in an array and console logs the first and last names of all the people. */
 
- var myStudents = [
-   {
-
-   firstName : "Alex",
-   lastName : "Smith",
-   age : 36
-},
+var myStudents = [
   {
-   firstName : "Oskar",
-   lastName : "Doe",
-   age : 30
-},
-  {   
-   firstName : "Ally",
-   lastName : "Young",                                             
-   age : 24,
-   
-}
+    firstName: "Alex",
+    lastName: "Smith",
+    age: 36,
+  },
+  {
+    firstName: "Oskar",
+    lastName: "Doe",
+    age: 30,
+  },
+  {
+    firstName: "Ally",
+    lastName: "Young",
+    age: 24,
+  },
 ];
 
 /*declare a forEach() method that accepts the function as a parameter*/
 
-/* the write a function that logs the first and last names*/
+/* then write a function that logs the first and last names*/
 
-
-myStudents.forEach(getNames)
-
+myStudents.forEach(getNames);
 
 function getNames(item) {
-   console.log(item.firstName + ' ' + item.lastName)
-
-};
-
-
-
-
-
-
-
-
- 
+  console.log(item.firstName + " " + item.lastName);
+}
